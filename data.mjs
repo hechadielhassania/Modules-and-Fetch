f271927876a9291908beceb87dddd5e737eb44fd
Hechadi El Hassania <hechadihassaia@gmail.com>
2024-04-15 15:03:49 -0400

    init commit

---
+++ data.mjs
@@ -0,0 +1,17 @@
+// Instance Files for new Instances of classes and funcCall()
+
+import Employee, { getName, getEmail, getPhone, fetchMe } from "./app.mjs";
+import { grabSprints } from "./sprint.mjs";
+import { getComments } from "./helper.mjs";
+
+let testEmployee = new Employee("Chester Tester", "@biz.gov", "000-000-0000");
+
+console.log(getName(testEmployee));
+console.log(getPhone(testEmployee));
+console.log(getEmail(testEmployee));
+// ----------------------------------------[INSTANCES]
+fetchMe();
+grabSprints()
+// getPhotos()
+console.log("getComments", getComments());
+// ----------------------------------------[Initialized_Funcs()]
